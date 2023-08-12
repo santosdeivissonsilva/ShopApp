5c77eca60e656af237715e083531c682f9d3f50e
santosdeivissonsilva <santos.deivissonsilva@hotmail.com>
2023-08-12 10:29:48 -0300

    first commit

---
+++ src/ShopApp.java
@@ -0,0 +1,8 @@
+public class ShopApp {
+    public static void main(String[] args) {
+        Clientes cliente1 = new Clientes();
+        cliente1.nome = "Paula";
+
+        System.out.println(cliente1.nome + ", seja bem-vinda ao Perfect Choice!");
+    }
+}
