a93ebb63c05f56d2606e5312e70bcb7412dd24b5
santosdeivissonsilva <santos.deivissonsilva@hotmail.com>
2023-08-12 11:51:38 -0300

    add new roupas

--- src/ShopApp.java
+++ src/ShopApp.java
@@ -10,31 +10,38 @@ public class ShopApp {
         System.out.println("Perfil: " + cliente1.nome);
         System.out.println("Seja bem-vinda (o) ao Perfect Choice!");
 
-        Roupas roupa1 = new Roupas();
-        Roupas roupa2 = new Roupas();
+        Roupas[] roupas = {new Roupas(), new Roupas(), new Roupas(), new Roupas()};
 
-        Roupas[] roupas = {roupa1, roupa2};
+        roupas[0].descricao = "Jaqueta Azul";
+        roupas[0].preco = 76.99;
+        roupas[0].tamanho = "M";
 
-        roupa1.descricao = "Jaqueta Azul";
-        roupa1.preco = 76.99;
-        roupa1.tamanho = "M";
+        roupas[1].descricao = "Camisa Laranja";
+        roupas[1].preco = 19.90;
+        roupas[1].tamanho = "P";
 
-        roupa2.descricao = "Camisa Laranja";
-        roupa2.preco = 19.90;
-        roupa2.tamanho = "P";
+        roupas[2].descricao = "Bermuda Verde";
+        roupas[2].preco = 15;
+        roupas[2].tamanho = "P";
+
+        roupas[3].descricao = "Camiseta Branca";
+        roupas[3].preco = 10.5;
+        roupas[3].tamanho = "P";
 
         int medida = 3;
 
         cliente1.tamanho = switch (medida) {
-            case 1, 2, 3 -> "S";
+            case 1, 2, 3 -> "P";
             case 4, 5, 6 -> "M";
             case 7, 8, 9 -> "G";
             default -> "X";
         };
         
-        for(Roupas roupa: roupas) {
-            precoTotal += roupa.preco;
-            System.out.println("Roupa: " + roupa.descricao + ", preço: R$" + roupa.preco + ", tamanho: " + roupa.tamanho);
+        for (Roupas roupa: roupas) {
+            if (cliente1.tamanho.equals(roupa.tamanho)){
+                precoTotal += roupa.preco;
+                System.out.println("Roupa: " + roupa.descricao + ", preço: R$" + roupa.preco + ", tamanho: " + roupa.tamanho);
+            }
         }
 
         System.out.println("O valor total a pagar é: R$" + precoTotal);
