9ac06c785a7128ba1064102461f1fbb9630ef6bb
santosdeivissonsilva <santos.deivissonsilva@hotmail.com>
2023-08-19 12:32:04 -0300

    change to init

--- src/ShopApp.java
+++ src/ShopApp.java
@@ -15,27 +15,27 @@ public class ShopApp {
         Roupas[] roupas = { new Roupas("Jaqueta Azul", 76.90, "M"), new Roupas("Camisa Laranja", 19.90, "P"),
                 new Roupas("Bermuda Verde", 15, "P"), new Roupas("Camiseta Branca", 10.5, "P") };
         
-        try {
-            ListaRoupas lista = new ListaRoupas(roupas);
-            Routing routing = Routing.builder().get("/lista", lista).build();
-            ServerConfiguration config = ServerConfiguration.builder().bindAddress(InetAddress.getLocalHost()).port(8888).build();
-            WebServer ws = WebServer.create(config, routing);
-            ws.start();
-        } catch (UnknownHostException ex) {
-            ex.printStackTrace();
+        // try {
+        //     ListaRoupas lista = new ListaRoupas(roupas);
+        //     Routing routing = Routing.builder().get("/lista", lista).build();
+        //     ServerConfiguration config = ServerConfiguration.builder().bindAddress(InetAddress.getLocalHost()).port(8888).build();
+        //     WebServer ws = WebServer.create(config, routing);
+        //     ws.start();
+        // } catch (UnknownHostException ex) {
+        //     ex.printStackTrace();
+        // }
+
+       System.out.println("Valor mínimo para compras: R$ " + Roupas.PRECO_MINIMO);
+
+       cliente1.adicionarRoupas(roupas);
+
+       Arrays.sort(cliente1.getRoupas());
+
+       for (Roupas roupa : roupas) {
+            System.out.println(roupa.toString());
         }
 
-    //    System.out.println("Valor mínimo para compras: R$ " + Roupas.PRECO_MINIMO);
-
-    //    cliente1.adicionarRoupas(roupas);
-
-    //    Arrays.sort(cliente1.getRoupas());
-
-    //    for (Roupas roupa : roupas) {
-    //         System.out.println(roupa.toString());
-    //     }
-
-    //     System.out.println("O valor total da compra foi: " + cliente1.getValorTotalCarrinho());
+        System.out.println("O valor total da compra foi: " + cliente1.getValorTotalCarrinho());
 
     }
 }
